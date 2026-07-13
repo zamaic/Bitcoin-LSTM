e4de17cef7dc1454fd7f5c49cfce03728eafa643
Samy <80605949+zamaic@users.noreply.github.com>
2026-07-13 12:36:09 -0600

    Update app title and introduction text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('BTC Price Forecasting with LSTM')
+st.write('Made by Samara Acosta')
+st.info('This app builds a Long-Short Term Mmeory RNN for BTC price forecasting!')
