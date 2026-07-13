4d4ab7dfa961fc81a4c9ca18543d30c59dc91ead
Samy <80605949+zamaic@users.noreply.github.com>
2026-07-13 12:49:40 -0600

    Update BTC data fill method
    Replaced NaN filling method from fillna to ffill for BTC data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
+import yfinance as yf
+
+# Introduction
 
 st.title('BTC Price Forecasting with LSTM')
 st.write('Made by Samara Acosta')
@@ -16,7 +19,6 @@ st.info('This app builds a Long-Short Term Mmeory RNN for BTC price forecasting!
 
 # Create CSV with historical data
 
-import yfinance as yf
 end_date =  datetime.today().strftime('%Y-%m-%d')
 
 btc = yf.download(
@@ -26,6 +28,6 @@ btc = yf.download(
 )
 
 btc.to_csv('bitcoin_historical_data.csv')
-btc.fillna(inplace=True)
+btc.ffill(inplace=True)
 df = pd.read_csv('bitcoin_historical_data.csv')
 df
