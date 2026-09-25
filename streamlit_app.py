1800fb864de7559152554a9133962e666f93da6f
Samy <80605949+zamaic@users.noreply.github.com>
2026-09-25 09:52:41 -0700

    Improve data display and add visualizations
    Refactor data handling and visualization in Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,12 @@ st.info('This app builds a Long-Short Term Mmeory RNN for BTC price forecasting!
 
 # Create CSV with historical data
 
-end_date =  datetime.today().strftime('%Y-%m-%d')
+end_date = datetime.today().strftime('%Y-%m-%d')
+
 btc = yf.download(
-"BTC-USD",
-start="2021-06-10",
-end = end_date
+    "BTC-USD",
+    start="2021-06-10",
+    end=end_date
 )
 
 btc.columns = btc.columns.droplevel(1)
@@ -36,14 +37,27 @@ btc.to_csv("bitcoin_historical_data.csv", index=False)
 with st.expander('Data'):
     st.write('**Raw Data**')
     df = pd.read_csv('bitcoin_historical_data.csv')
-    df
+    st.dataframe(df)
 
     st.write('**Features (X)**')
     st.write('Last 60 days (Open, High, Low, Close)')
-    X_display = df  'Date', 'Open', 'High', 'Low', 'Close'.tail(60)
-    X_display
+    X_display = df[['Date', 'Open', 'High', 'Low', 'Close']].tail(60)
+    st.dataframe(X_display)
     
     st.write('**Target (y)**')
     st.write('Close price of next day')
-    y_display = df  'Date', 'Close'.tail(60)
-    y_display
+    y_display = df[['Date', 'Close']].tail(60)
+    st.dataframe(y_display)
+
+with st.expander('Data Visualization'):
+    fig, ax = plt.subplots(figsize=(16,8))
+    ax.plot(btc[['Open','High','Low','Close']])
+    ax.set_title('Price Bitcoin - OHLC', fontsize=24)
+    ax.set_xlabel('Date', fontsize=18)
+    ax.set_ylabel('Price USD', fontsize=18)
+    ax.legend(['Open', 'High', 'Low', 'Close'])
+    ax.grid(True)
+    st.pyplot(fig)
+    
+    st.write('**Close Price**')
+    st.line_chart(btc.set_index('Date')['Close'])
